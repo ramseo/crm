46b1c21026f8dba38616f67d4d7592124186161c
vickyorigin <vickywork55@gmail.com>
2023-03-17 15:42:37 +0530

    add base url

--- constant/layout/head.php
+++ constant/layout/head.php
@@ -11,6 +11,20 @@
   <meta name="description" content="This Penglead Lead management System Developed by Mayuri K. Freelancer in india">
   <meta name="keywords" content="Mayuri K.freelancer in india">
   <meta name="author" content="Mayuri K">
+  <?php
+
+  function url()
+  {
+    return sprintf(
+      "%s://%s%s",
+      isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+      $_SERVER['SERVER_NAME'],
+      $_SERVER['REQUEST_URI']
+    );
+  }
+
+  ?>
+  <base href="<?= url() ?>">
 
   <link rel="icon" type="image/png" sizes="16x16" href="assets/uploadImage/Logo/favicon.png">
   <?php
@@ -22,19 +36,19 @@
   ?>
   <title>Penglead Lead management System</title>
 
-  <link href="<?= BASE_URL ?>assets/css/lib/chartist/chartist.min.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>assets/css/lib/owl.carousel.min.css" rel="stylesheet" />
-  <link href="<?= BASE_URL ?>assets/css/lib/owl.theme.default.min.css" rel="stylesheet" />
+  <link href="assets/css/lib/chartist/chartist.min.css" rel="stylesheet">
+  <link href="assets/css/lib/owl.carousel.min.css" rel="stylesheet" />
+  <link href="assets/css/lib/owl.theme.default.min.css" rel="stylesheet" />
 
-  <link href="<?= BASE_URL ?>assets/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
 
-  <link href="<?= BASE_URL ?>assets/css/helper.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>assets/css/style.css" rel="stylesheet">
-  <link rel="<?= BASE_URL ?>stylesheet" href="assets/css/lib/html5-editor/bootstrap-wysihtml5.css" />
-  <link href="<?= BASE_URL ?>assets/css/lib/calendar2/semantic.ui.min.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>assets/css/lib/calendar2/pignose.calendar.min.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>assets/css/lib/sweetalert/sweetalert.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>assets/css/lib/datepicker/bootstrap-datepicker3.min.css" rel="stylesheet">
+  <link href="assets/css/helper.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/lib/html5-editor/bootstrap-wysihtml5.css" />
+  <link href="assets/css/lib/calendar2/semantic.ui.min.css" rel="stylesheet">
+  <link href="assets/css/lib/calendar2/pignose.calendar.min.css" rel="stylesheet">
+  <link href="assets/css/lib/sweetalert/sweetalert.css" rel="stylesheet">
+  <link href="assets/css/lib/datepicker/bootstrap-datepicker3.min.css" rel="stylesheet">
   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
 
